8d64f1f2b156b48045c2c2c2a0bc742bd6b496bb
jeweltot <mynamewys@gmail.com>
2019-02-09 14:40:45 +0900

    - 최초 업로드

---
+++ src/main/java/me/jeweltot/springboot008/Springboot008Application.java
@@ -0,0 +1,16 @@
+package me.jeweltot.springboot008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot008Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Springboot008Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/me/jeweltot/springboot008/Springboot008ApplicationTests.java
@@ -0,0 +1,19 @@
+package me.jeweltot.springboot008;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot008ApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
+
